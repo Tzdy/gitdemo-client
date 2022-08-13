1218162dfece058f9bdafcd2c1e699b77eb01225
Tsdy <571219188@qq.com>
2022-08-13 18:09:03 +0800

    build: 修改依赖列表，回退nuxt-rc.6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,23 @@
 import { defineNuxtConfig } from "nuxt";
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import path from 'path'
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import path from "path";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/gitdemo/'
+    baseURL: "/gitdemo/",
   },
   typescript: {
     shim: false,
   },
-  modules: ["@lewebsimple/nuxt3-svg", '@pinia/nuxt'],
+  modules: ["@pinia/nuxt"],
   vite: {
     plugins: [
       createSvgIconsPlugin({
         // 指定需要缓存的图标文件夹
-        iconDirs: [path.resolve(process.cwd(), 'icons')],
+        iconDirs: [path.resolve(process.cwd(), "icons")],
         // 指定symbolId格式
-        symbolId: 'icon-[dir]-[name]',
+        symbolId: "icon-[dir]-[name]",
       }),
-    ]
-  }
+    ],
+  },
 });
